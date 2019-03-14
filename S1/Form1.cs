dde060638dcc4aea9868d10f46313631fe838196
mustafasheikh1 <mustafaahmed726@gmail.com>
2019-03-14 05:30:32 +0500

    changes

--- S1/Form1.cs
+++ S1/Form1.cs
@@ -12,7 +12,7 @@ namespace S1
 {
     public partial class Form1 : Form
     {
-        public int R, G, B = 0;
+       
 
         public Form1()
         {
@@ -23,7 +23,7 @@ namespace S1
         {
             OpenFileDialog file = new OpenFileDialog();
             Bitmap bmp;
-            if(file.ShowDialog() == DialogResult.OK)
+            if (file.ShowDialog() == DialogResult.OK)
             {
                 bmp = new Bitmap(file.FileName);
                 pictureBox1.Image = bmp;
@@ -33,221 +33,101 @@ namespace S1
 
         private void button2_Click(object sender, EventArgs e)
         {
-            this.R = 0;
-            this.G = 0;
-            this.B = 0;
-            LinkedList<Obj> list = new LinkedList<Obj>();
-
+            
             Bitmap bmp = new Bitmap(pictureBox1.Image);
-            Bitmap temp = new Bitmap(pictureBox1.Image);
-            temp = greyScale(temp);
+            int[,] label = new int[bmp.Height, bmp.Width];
+            int[,] upper = new int[bmp.Height, bmp.Width];
+            int[,] left = new int[bmp.Height, bmp.Width];
+            
 
-            for (int y = 0; y < temp.Height; y++)
+            int index = 1;
+            
+            //Nested For loops for horizontal Scan
+            for(int i = 1; i < bmp.Height-1; i++)
             {
-                for (int x = 0; x < temp.Width; x++)
+                for(int j = 1; j < bmp.Width-1; j++)
                 {
                     
-                    if (temp.GetPixel(x,y) == Color.FromArgb(0,0,0))
+                    if (bmp.GetPixel(j, i) == Color.Black || bmp.GetPixel(j, i).R < 10)
                     {
-
-                        if (!caseHandeler(temp, x, y))
+                        upper[i, j] = label[i - 1, j];
+                        left[i, j]= label[i, j - 1];
+                        if(upper[i,j] == 0 && left[i,j] == 0)
                         {
-                            temp = indexPixel(temp, x, y);
-                            Obj obj = new Obj();
-                            obj.list.AddFirst(Color.FromArgb(temp.GetPixel(x, y).R, temp.GetPixel(x, y).G, temp.GetPixel(x, y).B));
-                            list.AddLast(new Obj());
-                            
-                        }else
+                            label[i, j] = index;
+                            index++;
+                        }else if (upper[i, j] == 0 && left[i, j] != 0 || upper[i, j] != 0 && left[i, j] == 0)
                         {
-                            Color color = newColor(temp, x, y);
-                            Console.WriteLine(color.R + "," + color.G + "," + color.B);
+                            if(upper[i,j] != 0)
+                            {
+                                label[i, j] = upper[i, j];
+                                //Console.WriteLine("UHit");
+                            }
+                            else
+                            {
+                                label[i, j] = left[i, j];
+                                //Console.WriteLine("LHit");
 
-                            temp.SetPixel(x,y, color);
+                            }
+
+                        }
+                        else if(left[i,j] == upper[i, j] && left[i,j] != 0 && upper[i,j] != 0)
+                        {
+                            //Console.WriteLine("Same");
+                            label[i, j] = left[i, j];
+                        }else if (left[i,j] != upper[i,j] && left[i, j] != 0 && upper[i, j] != 0)
+                        {
+                            ///Console.WriteLine("Not Same");
+                            if (upper[i,j] < left[i, j])
+                            {
+                                label[i, j] = upper[i, j];
+                                
+                            }
+                            else
+                            {
+                                label[i, j] = left[i, j];
+                                    
+                            }
+
+                            //index--;
                         }
                     }
                 }
             }
 
 
+            int max = label.Cast<int>().Max();
 
-            for (int x = 0; x < temp.Width; x++)
+            Console.WriteLine(max);
+
+
+            Color[] ColorList = new Color[max];
+
+
+            Random rand = new Random();
+            for(int i = 0; i < max; i++)
             {
-                for(int y = 0; y < temp.Height; y++)
+                ColorList[i] = Color.FromArgb(rand.Next(0,256), rand.Next(0, 256), rand.Next(0, 256));
+            }
+
+            for(int y = 0; y < bmp.Height; y++)
+            {
+                for(int x = 0; x < bmp.Width; x++)
                 {
-                    if(temp.GetPixel(x,y).B == 1  || temp.GetPixel(x, y).B < 255) 
+                    if(label[y,x] != 0)
                     {
-                        
-                        temp.SetPixel(x, y, Color.Blue);
+                        bmp.SetPixel(x, y, ColorList[label[y,x]-1]);
                     }
-                    
                 }
             }
 
-            Console.WriteLine(this.B);
-            Console.WriteLine(list.Count);
-            pictureBox1.Image = temp;
-
+            pictureBox1.Image = bmp;
             
         }
 
 
-        private Color newColor(Bitmap bmp, int x, int y)
-        {
-            int max = 0;
-            int total = 0;
-
-            //for left pixel
-            if (bmp.GetPixel(x - 1, y) != Color.FromArgb(255, 255, 255))
-            {
-                total = bmp.GetPixel(x - 1, y).R + bmp.GetPixel(x - 1, y).G + bmp.GetPixel(x - 1, y).B;
-
-                if (total > max)
-                {
-                    max = 1;
-                }
-            }
-
-
-            if (bmp.GetPixel(x - 1, y - 1) != Color.FromArgb(255, 255, 255))
-            {
-
-                total = bmp.GetPixel(x - 1, y - 1).R + bmp.GetPixel(x - 1, y - 1).G + bmp.GetPixel(x - 1, y - 1).B;
-
-                if (total > max)
-                {
-                    max = 2;
-                }
-            }
-
-
-            if (bmp.GetPixel(x, y - 1) != Color.FromArgb(255, 255, 255))
-            {
-                total = bmp.GetPixel(x, y - 1).R + bmp.GetPixel(x, y - 1).G + bmp.GetPixel(x, y - 1).B;
-                if (total > max)
-                {
-                    max = 3;
-                }
-            }
-
-
-            if (bmp.GetPixel(x + 1, y - 1) != Color.FromArgb(255, 255, 255))
-            {
-                total = bmp.GetPixel(x + 1, y - 1).R + bmp.GetPixel(x + 1, y - 1).G + bmp.GetPixel(x + 1, y - 1).B;
-                if (total > max)
-                {
-                    max = 4;
-
-                }
-            }
-                
-
-            //Console.WriteLine(max);
-            Color color;
-            switch (max)
-            {
-                case 1:
-                    color = Color.FromArgb(bmp.GetPixel(x - 1, y).R, bmp.GetPixel(x - 1, y).G, bmp.GetPixel(x - 1, y).B);
-                    return color;
-                case 2:
-                    color = Color.FromArgb(bmp.GetPixel(x - 1, y - 1).R, bmp.GetPixel(x - 1, y - 1).G, bmp.GetPixel(x - 1, y - 1).B);
-                    return color;
-                case 3:
-                    color = Color.FromArgb(bmp.GetPixel(x, y - 1).R, bmp.GetPixel(x, y - 1).G, bmp.GetPixel(x, y - 1).B);
-                    return color;
-                case 4:
-                    color = Color.FromArgb(bmp.GetPixel(x + 1, y - 1).R, bmp.GetPixel(x + 1, y - 1).G, bmp.GetPixel(x + 1, y - 1).B);
-                    return color;
-                default:
-                    return Color.Black;
-            }
-
-
-        }
-
-        //Clear
-        private Bitmap indexPixel(Bitmap bmp, int x , int y)
-        {
-            if(this.B < 255)
-            {
-                this.B = this.B + 1;
-            }
-            else if (this.G < 255)
-            {
-                this.G = this.G + 1;
-            }else if(this.R < 255)
-            {
-                this.R = this.R + 1;
-            }
-
-            bmp.SetPixel(x, y, Color.FromArgb(R, G, B));
-            return bmp;
-        }
-
-
-        //CLear
-        private bool caseHandeler(Bitmap bmp, int x , int y)
-        {
-           
-            if(bmp.GetPixel(x-1,y) != Color.FromArgb(255,255,255))
-            {
-                return true;
-            }else if(bmp.GetPixel(x - 1, y-1) != Color.FromArgb(255, 255, 255))
-            {
-                return true;
-            }
-            else if (bmp.GetPixel(x , y - 1) != Color.FromArgb(255, 255, 255))
-            {
-                return true;
-            }
-            else if (bmp.GetPixel(x + 1, y - 1) != Color.FromArgb(255, 255, 255))
-            {
-                return true;
-            }
-
-            return false;
-        }
-
         
-        //Clear
-        private Bitmap greyScale(Bitmap bmp)
-        {
-            for (int w = 0; w < bmp.Width; w++)
-            {
-                for (int h = 0; h < bmp.Height; h++)
-                {
-                    int total;
-                    int red = bmp.GetPixel(w, h).R;
-                    int green = bmp.GetPixel(w, h).G;
-                    int blue = bmp.GetPixel(w, h).B;
-
-                    total = (red + green + blue) / 3;
-
-                    bmp.SetPixel(w, h, Color.FromArgb(total, total, total));
-                }
-            }
-
-            for (int w = 0; w < bmp.Width; w++)
-            {
-                for (int h = 0; h < bmp.Height; h++)
-                {
-                    if (bmp.GetPixel(w, h) == Color.Black || bmp.GetPixel(w, h).R < 200)
-                    {
-                        bmp.SetPixel(w, h, Color.FromArgb(0,0,0));
-                    }
-                    else
-                    {
-                        bmp.SetPixel(w, h, Color.White);
-                    }
-                }
-            }
-
-            return bmp;
-        }
     }
 
-
-    class Obj
-    {
-        public LinkedList<Color> list = new LinkedList<Color>();
-    }
+    
 }
